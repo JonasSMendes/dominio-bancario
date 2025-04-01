16da96b933b9698c2fbfb56be0ded2a550c743db
jonas mendes <jonas.jsm2903@gmail.com>
2025-04-01 06:49:08 -0300

    Iniciando projeto

---
+++ src/main/java/me/JonasSmendes/dominioBancario/DominioBancarioApplication.java
@@ -0,0 +1,13 @@
+package me.JonasSmendes.dominioBancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioBancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/JonasSmendes/dominioBancario/DominioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.JonasSmendes.dominioBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
